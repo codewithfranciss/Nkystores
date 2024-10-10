f26880a3e351da94339e517ff31dc567f5f32446
codewithfrancis <offor919@gmail.com>
2024-10-10 10:22:48 +0100

    Added supabase client for database connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { supabase } from './lib/supabaseClient'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function  App() {
 
+  
   return (
     <>
       <div className='text-center'>Hello</div>
